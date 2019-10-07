b01ef9c6814229010834f5597907c625e9cec3c7
Roshan Patil <roshanpatil3004@gmail.com>
2019-10-07 12:22:37 +0530

    Added comments to understand the code

--- executeCommandInsideConatiner.py
+++ executeCommandInsideConatiner.py
@@ -1,3 +1,9 @@
+"""
+The below code is api version of the kubectl command exec
+eg :
+ kubectl exec pod-name exec command
+"""
+
 from os import path
 
 from kubernetes import client, config, utils
@@ -14,6 +20,7 @@ def execute_on_container(core_api_instance,pod,namespace,exec_command1):
 
 config.load_kube_config()
 namespace = "default"
+#Pod name on which commands is to be executed
 pod = "termi-demo-6bf977984f-f254l"
 exec_command1 = ['bash','/abc/t1.sh']
 core_api_instance = client.CoreV1Api()
